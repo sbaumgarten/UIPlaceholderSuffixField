03f8ccf5585ed68912372e660eb53b411d2c17c1
Sam Baumgarten <sbaumgarten@makrr.com>
2013-01-01 17:08:37 -0800

    Removed annoying NSLog

--- UIPlaceholderSuffixField.m
+++ UIPlaceholderSuffixField.m
@@ -21,10 +21,7 @@
 
 -(void)drawPlaceholderInRect:(CGRect)rect {}
 
-- (void)drawRect:(CGRect)rect {
-    NSLog(@"x: %f :: y: %f :: w: %f :: h: %f", rect.origin.x, rect.origin.y, rect.size.width, rect.size.height);
-    
-    // Set colour and font size of placeholder text
+- (void)drawRect:(CGRect)rect {    
     [[UIColor colorWithRed:197.0/255.0 green:197.0/255.0 blue:197.0/255.0 alpha:1.0] setFill];
     
     CGFloat x;
